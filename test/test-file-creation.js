5675f622589202602f7b9975400139c27efb48a7
Balthazar Gronon <bgronon@gmail.com>
2014-07-14 18:35:08 +0200

    test(timeout): more timeout for test

--- test/test-file-creation.js
+++ test/test-file-creation.js
@@ -34,7 +34,7 @@ describe('angular-fullstack generator', function () {
   }
 
   beforeEach(function (done) {
-    this.timeout(10000);
+    this.timeout(30000);
     var deps = [
       '../../app',
       [
